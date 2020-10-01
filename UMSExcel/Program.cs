e01e6d1959a045b98c30e7c018150aa7925c81c0
klopez26 <klopez26@DXCUSAAI421607>
2020-10-01 10:16:38 -0400

    Add project files.

---
+++ UMSExcel/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace UMSExcel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["UMS"].ConnectionString);
+            SqlCommand sqlCommand = new SqlCommand("SELECT * FROM ExcelGenerate", sqlConnection);
+            SqlCommand sqlCommand2 = new SqlCommand("SELECT * FROM UserInfo",sqlConnection);
+            SqlCommand sqlCommandDel = new SqlCommand("DELETE FROM ExcelGenerate",sqlConnection);
+            /*sqlCommand.CommandType = CommandType.StoredProcedure;*/
+            /*sqlCommand.Parameters.AddWithValue("@UserId", userId);*/
+            sqlConnection.Open();
+            var reader = sqlCommand.ExecuteReader();
+
+            if (reader.Read())
+            {
+                string fileName = reader.GetString(0);
+                string sendEmailTo = reader.GetString(1);
+               
+                sqlConnection.Close();
+
+                sqlConnection.Open();
+                var getAll = sqlCommand2.ExecuteReader();
+
+                string OpenAtDownload = "";
+
+
+                /* System.Diagnostics.Debug.WriteLine("Is it empty: " + fileName.IsNullOrWhiteSpace());
+                 if (fileName == "" || fileName.IsNullOrWhiteSpace() == true) { fileName = "UserList"; }*/
+                if (fileName == "" || fileName == null) { fileName = "UserList"; }
+                
+                Microsoft.Office.Interop.Excel.Workbook ExcelWorkBook;
+                Microsoft.Office.Interop.Excel.Worksheet ExcelWorkSheet;
+                object misValue = System.Reflection.Missing.Value;
+                Microsoft.Office.Interop.Excel.Application ExcelExport = new Microsoft.Office.Interop.Excel.Application();
+
+                ExcelWorkBook = ExcelExport.Workbooks.Add(misValue);
+                ExcelWorkSheet = ExcelWorkBook.Worksheets.get_Item(1);
+
+                ExcelWorkSheet.Cells.NumberFormat = "@";
+                var rowColor = System.Drawing.ColorTranslator.ToOle(System.Drawing.Color.Yellow);
+
+                ExcelWorkSheet.get_Range("A1", "K1").Interior.Color = rowColor;
+                ExcelWorkSheet.get_Range("A1", "K1").Font.Bold = true;
+
+
+                ExcelWorkSheet.Cells[1, 1] = "UserName";
+
+                ExcelWorkSheet.Cells[1, 2] = "FirstName";
+
+                ExcelWorkSheet.Cells[1, 3] = "LastName";
+
+                ExcelWorkSheet.Cells[1, 4] = "Email";
+
+                ExcelWorkSheet.Cells[1, 5] = "Address";
+
+                ExcelWorkSheet.Cells[1, 6] = "City";
+
+                ExcelWorkSheet.Cells[1, 7] = "Country";
+
+                ExcelWorkSheet.Cells[1, 8] = "ZipCode";
+
+                ExcelWorkSheet.Cells[1, 9] = "PhoneNumber";
+
+                ExcelWorkSheet.Cells[1, 10] = "Role";
+
+                ExcelWorkSheet.Cells[1, 11] = "Status";
+
+                /*  var userInfoes = db.UserInfoes.Include(u => u.Role).Include(u => u.Status).Include(u => u.UserCredential);
+                  userInfoes.ToList();*/
+                int row = 2;
+                while (getAll.Read())
+                {
+
+                    ExcelWorkSheet.Cells[row, 1] = getAll.GetString(1);
+                    ExcelWorkSheet.Cells[row, 2] = getAll.GetString(2);
+                    ExcelWorkSheet.Cells[row, 3] = getAll.GetString(3);
+                    ExcelWorkSheet.Cells[row, 4] = getAll.GetString(4);
+                    ExcelWorkSheet.Cells[row, 5] = getAll.GetString(5);
+                    ExcelWorkSheet.Cells[row, 6] = getAll.GetString(6);
+                    ExcelWorkSheet.Cells[row, 7] = getAll.GetString(7);
+                    ExcelWorkSheet.Cells[row, 8] = getAll.GetString(8);
+                    ExcelWorkSheet.Cells[row, 9] = getAll.GetString(9);
+                    ExcelWorkSheet.Cells[row, 10] = getAll.GetInt32(10);
+                    ExcelWorkSheet.Cells[row, 11] = getAll.GetInt32(11);
+                    row++;
+                }
+                ExcelWorkSheet.Columns.AutoFit();
+                /*string isEmailSend = "";*/
+                sqlConnection.Close();
+                sqlConnection.Open();
+                try
+                {
+                    ExcelWorkBook.SaveAs(fileName, Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+
+                    if (sendEmailTo != null)
+                    {
+                        try
+                        {
+                            ExcelWorkBook.SendMail(sendEmailTo, "User List From the User Management System");
+                            /*isEmailSend = "Email was sent.";*/
+                        }
+                        catch
+                        {
+                            /*isEmailSend = "Email was not sent.";*/
+                        }
+
+                    }
+
+                    if (OpenAtDownload == "true")
+                    {
+                        ExcelWorkBook.WebPagePreview();
+                        /*System.Diagnostics.Process.Start($"E:/Documents/{fileName}.xls"); */
+                    }
+                    /*ExcelWorkBook.WebPagePreview();*/
+                    ExcelWorkBook.Close(true, misValue, misValue);
+
+                    ExcelExport.Quit();
+
+                    Marshal.ReleaseComObject(ExcelWorkSheet);
+                    Marshal.ReleaseComObject(ExcelWorkBook);
+                    Marshal.ReleaseComObject(ExcelExport);
+                    sqlCommandDel.ExecuteReader();
+                    sqlConnection.Close();
+                    /*return RedirectToAction("Index", new { a = "Excel Document was created sucessfully and it should be available in your Documents folder.", color = "green", emailStatus = isEmailSend });*/
+                }
+                catch
+                {
+                    ExcelWorkBook.Close(true, misValue, misValue);
+
+                    ExcelExport.Quit();
+
+
+                    Marshal.ReleaseComObject(ExcelWorkSheet);
+                    Marshal.ReleaseComObject(ExcelWorkBook);
+                    Marshal.ReleaseComObject(ExcelExport);
+
+                    System.Diagnostics.Debug.WriteLine("Excel Document was not created.");
+                    sqlCommandDel.ExecuteReader();
+                    sqlConnection.Close();
+                    /*return RedirectToAction("Index", new { a = "Excel Document was not created.", color = "red", emailStatus = isEmailSend });*/
+
+                }
+            }
+
+            }
+    }
+}
+
---
+++ UMSExcel/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("UMSExcel")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UMSExcel")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f85f2e0c-de51-44e3-bc4f-134d5d801598")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
